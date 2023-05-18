2e8b37ac3a78331078ac94622e83be8d332d5744
Karolline <hj264@hanmail.net>
2023-05-19 08:56:44 +0900

    표지 margin top 조정

--- src/pages/feeds/components/feed/HighlightModal.tsx
+++ src/pages/feeds/components/feed/HighlightModal.tsx
@@ -112,7 +112,7 @@ const scale = useTransform(x, [-150, 0, 150], [0.5, 1, 0.5]);
 }
 
 const StyledMotionDiv = styled(motion.div, {
-  // marginTop: `calc(10vh)`,
+  marginTop: `calc(25vh - 100px)`,
   position: `absolute`,
   zIndex: '$max1',
 });
