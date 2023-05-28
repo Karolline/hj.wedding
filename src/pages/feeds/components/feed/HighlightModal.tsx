522b71b5c6f27952fc1e7cb0a2e6b58a4d6f3eef
Karolline <hj264@hanmail.net>
2023-05-28 17:03:31 +0900

    기존방식으로 돌아와 vh계산적용 제외 (빌드실패)

--- src/pages/feeds/components/feed/HighlightModal.tsx
+++ src/pages/feeds/components/feed/HighlightModal.tsx
@@ -27,14 +27,13 @@ export function HighlightModal(props: any) {
   const 표지_이미지_소스 = '/assets/img/highlight_'+표지_이미지_넘버+'.png'
   const 표지_이미지_블러 = `/_next/image?url=${표지_이미지_소스}&w=16&q=1` // next image라 SSR인 듯
 
-  function setScreenSize() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }
+  // function setScreenSize() {
+  //   document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
+  // }
 
-  useEffect(() => {
-    setScreenSize();
-  });
+  // useEffect(() => {
+  //   setScreenSize();
+  // });
 
 
 /* Prop `style` did not match
@@ -122,8 +121,8 @@ const StyledMotionDiv = styled(motion.div, {
 const StyledMotionWrapper = styled(motion.section, {
   backgroundColor: '$transparent',
 
-  // height: '100vh',
-  height: 'calc(var(--vh, 1vh) * 100',
+  height: '100vh',
+  // height: 'calc(var(--vh, 1vh) * 100',
 
   position: 'absolute',
   top: 0,
